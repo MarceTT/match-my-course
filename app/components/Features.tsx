5106396105b088a8d4d12d95b4f87c75b4e39a4c
MarceTT <mtoro6@gmail.com>
2025-02-10 18:41:06 -0600

    cambio en url del detalle de servicio

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -26,7 +26,7 @@ const Features = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {features.map((feature, i) => (
           <div key={i} className="flex flex-col md:flex-row items-center md:items-start text-center md:text-left">
-            <Image src={feature.image} width={48} height={48} className="mb-4 md:mb-0 md:mr-4 flex-shrink-0" alt={feature.text} />
+            <Image src={feature.image} width={48} height={48} className="mb-4 md:mb-0 md:mr-4 flex-shrink-0 hover:text-color hover:bg-color hover:bg-[#FFDB5B]" alt={feature.text} />
             <p className="text-md text-gray-700 lg:mt-2">{feature.text}</p>
           </div>
         ))}
--- app/components/VideoList.tsx
+++ app/components/VideoList.tsx
@@ -102,7 +102,7 @@ interface Video {
     return (
       <Card className="overflow-hidden bg-white rounded-lg shadow-md transition-all duration-300 hover:shadow-xl hover:scale-105">
         <CardContent className="p-0">
-          <Link href={`http://localhost:3000/servicios/${videoId}`} className="block relative aspect-video group">
+          <Link href={`/servicios/${videoId}`} className="block relative aspect-video group">
             <Image src={imageError ? "/placeholder.svg" : thumbnailUrl} alt={video.title} className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" width={640} height={480} onError={() => setImageError(true)} />
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-40 transition-all duration-300">
               <div className="w-12 h-12 rounded-full bg-black/60 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 transform group-hover:scale-110">
