b03fcf51959719fb89fde91a0121d803a82559cf
MarceTT <mtoro6@gmail.com>
2025-04-03 13:54:34 -0300

    cambios en middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get("refreshToken")?.value;
   const { pathname } = request.nextUrl;
 
@@ -16,24 +15,9 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // Validar refreshToken si existe
-  if (refreshToken) {
-    try {
-      const secret = new TextEncoder().encode(process.env.JWT_REFRESH_SECRET!);
-      await jwtVerify(refreshToken, secret);
-
-      // Ya está logueado y va al login → redirigir al dashboard
-      if (pathname === "/login") {
-        return NextResponse.redirect(new URL("/admin/dashboard", request.url));
-      }
-
-      // Todo OK, continuar
-      return NextResponse.next();
-    } catch (err) {
-      console.warn("⛔ RefreshToken inválido o expirado en middleware");
-      // Redirige a login si el token no es válido
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  // Ya está logueado y va al login → redirigir al dashboard
+  if (refreshToken && pathname === "/login") {
+    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
   }
 
   return NextResponse.next();
