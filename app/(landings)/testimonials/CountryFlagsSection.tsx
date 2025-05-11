588554c425f77fbee3f4226fa453fcba8d0f94cb
Oscar Osses <oscarossesa@gmail.com>
2025-05-11 15:45:59 +0100

    feature: adding flags in testimonials page

--- app/(landings)/testimonials/CountryFlagsSection.tsx
+++ app/(landings)/testimonials/CountryFlagsSection.tsx
@@ -21,7 +21,7 @@ const CountryFlagsSection = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
-                src="https://flagcdn.com/w40/ie.png"
+                src="/flags/irlanda.png"
                 alt="Irlanda"
                 width={300}
                 height={300}
@@ -35,10 +35,10 @@ const CountryFlagsSection = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
-                src="https://flagcdn.com/w40/nz.png"
+                src="/flags/nueva-zelanda.png"
                 alt="Nueva Zelanda"
-                width={40}
-                height={40}
+                width={300}
+                height={300}
                 className="object-cover"
               />
             </div>
@@ -49,10 +49,10 @@ const CountryFlagsSection = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
-                src="https://flagcdn.com/w40/ae.png"
-                alt="Dubai"
-                width={40}
-                height={40}
+                src="/flags/dubai.png"
+                alt="dubai"
+                width={300}
+                height={300}
                 className="object-cover"
               />
             </div>
@@ -63,10 +63,10 @@ const CountryFlagsSection = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
-                src="https://flagcdn.com/w40/ca.png"
-                alt="Canadá"
-                width={40}
-                height={40}
+                src="/flags/canada.png"
+                alt="dubai"
+                width={300}
+                height={300}
                 className="object-cover"
               />
             </div>
@@ -77,11 +77,13 @@ const CountryFlagsSection = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 rounded-full overflow-hidden">
               <Image
-                src="https://flagcdn.com/w40/gb.png"
-                alt="Reino Unido"
-                width={40}
-                height={40}
-                className="object-cover"
+                src="/flags/reino-unido.png"
+                alt="Cork English Academy"
+                width={400}
+                height={400}
+                priority
+                sizes="(max-width: 768px) 100vw, 400px"
+                className="rounded-sm"
               />
             </div>
             <span className="text-sm mt-2">Reino Unido</span>
