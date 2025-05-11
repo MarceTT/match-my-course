eed6cc344f64363201161b54be17d3a9ae3e3d82
Oscar Osses <oscarossesa@gmail.com>
2025-05-11 16:31:48 +0100

    feat: testimonials image to landing page added.

--- app/(landings)/testimonials/HeroSection.tsx
+++ app/(landings)/testimonials/HeroSection.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <section className="relative h-96 bg-cover bg-center" style={{ backgroundImage: "url('/images/2.png')" }}>
+    <section className="relative h-96 bg-cover bg-center"
+      style={{ backgroundImage: "url('/landing-pages/Testimonios.png')" }}
+    >
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <h1 className="text-white text-4xl md:text-5xl font-bold text-shadow">
           Testimonios
