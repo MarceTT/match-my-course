e5682b87b2d07ed1f26261e02bd2f887bc0dfc00
Oscar Osses <oscarossesa@gmail.com>
2025-05-14 00:10:28 +0100

    feat: partners updated

--- app/(landings)/partners/CountrySection.tsx
+++ app/(landings)/partners/CountrySection.tsx
@@ -1,12 +1,13 @@
 import { Partner } from "@/app/lib/partners";
-import { PartnerCard } from "./PartnerCard";
+import Image from "next/image";
 
 interface Props {
+  countryKey: string;
   name: string;
   partners: Partner[];
 }
 
-export default function CountrySection({ name, partners }: Props) {
+export default function CountrySection({ countryKey, name, partners }: Props) {
   return (
     <div className="space-y-6">
       <div className="text-left">
@@ -14,9 +15,21 @@ export default function CountrySection({ name, partners }: Props) {
         <hr className="my-4 border-t-2 border-gray-300 w-full" />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-        {partners.map((partner: Partner) =>
-          <PartnerCard key={partner.src} partner={partner} />
-        )}
+        {partners
+          .filter((partner: Partner) => partner.key === countryKey)
+          .map((partner: Partner) => (
+            <div key={partner.src} className="flex justify-center items-center">
+              <Image
+                src={partner.src}
+                alt={partner.alt}
+                width={400}
+                height={400}
+                priority
+                sizes="(max-width: 768px) 100vw, 400px"
+                className="rounded-sm"
+              />
+            </div>
+          ))}
       </div>
     </div>
   )
