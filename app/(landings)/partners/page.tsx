3719613068951946c426d071e8abc1e95b211e1f
Oscar Osses <oscarossesa@gmail.com>
2025-05-31 13:52:03 +0100

    chore: types updates

--- app/(landings)/partners/page.tsx
+++ app/(landings)/partners/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import { Partner } from '@/app/lib/types'
+import { Partner } from '@/types'
 import { getAllPartners } from '@/app/lib/api/partners'
 import HeaderSection from './HeaderSection'
 import PartnerSection from './PartnerSection'
--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,4 @@
+export * from "./partner";
+export * from "./reservation";
 export * from "./service";
+export * from "./testimonial";
