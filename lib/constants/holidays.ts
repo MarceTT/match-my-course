9cd7d09746c1e976eaad5f29065637837733b431
Oscar Osses <oscarossesa@gmail.com>
2025-06-15 21:53:39 +0100

    feat: calendar implemented in reservation form

---
+++ lib/constants/holidays.ts
@@ -0,0 +1,24 @@
+export const irishHolidays: Date[] = [
+  // 2025
+  new Date(2025, 0, 1),  // Año Nuevo
+  new Date(2025, 2, 17), // Día de San Patricio
+  new Date(2025, 3, 21), // Viernes Santo (Good Friday) *No es feriado oficial nacional, pero muchas instituciones lo observan
+  new Date(2025, 4, 5),  // Primer lunes de mayo (May Day)
+  new Date(2025, 5, 2),  // Primer lunes de junio (June Bank Holiday)
+  new Date(2025, 5, 16),  // test
+  new Date(2025, 7, 4),  // Primer lunes de agosto (August Bank Holiday)
+  new Date(2025, 9, 27), // Último lunes de octubre (October Bank Holiday)
+  new Date(2025, 11, 25), // Navidad
+  new Date(2025, 11, 26), // Día de San Esteban (St. Stephen’s Day / Boxing Day)
+
+  // 2026
+  new Date(2026, 0, 1),  // Año Nuevo
+  new Date(2026, 2, 17), // Día de San Patricio
+  new Date(2026, 3, 6),  // Lunes de Pascua (Easter Monday)
+  new Date(2026, 4, 4),  // Primer lunes de mayo (May Day)
+  new Date(2026, 5, 1),  // Primer lunes de junio (June Bank Holiday)
+  new Date(2026, 7, 3),  // Primer lunes de agosto (August Bank Holiday)
+  new Date(2026, 9, 26), // Último lunes de octubre (October Bank Holiday)
+  new Date(2026, 11, 25), // Navidad
+  new Date(2026, 11, 28), // Día de San Esteban (observado el lunes siguiente al 26)
+];
