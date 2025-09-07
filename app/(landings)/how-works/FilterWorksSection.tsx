c2458df86173dfd2396ea64cd46c77a661ccdf10
MarceTT <mtoro6@gmail.com>
2025-09-07 12:28:54 -0300

    Cmabios en imagen de como funciona

--- app/(landings)/how-works/FilterWorksSection.tsx
+++ app/(landings)/how-works/FilterWorksSection.tsx
@@ -57,7 +57,7 @@ const FilterWorksSection = () => {
           {/* Left Column - Filter Demo (se muestra abajo en mobile) */}
           <div className="order-2 md:order-1 space-y-6">
             <Image
-              src="/about-us/¿Cómo funciona el filtro de MatchMyCourse.png"
+              src="/about-us/Como_funciona_el_filtro_de_MatchMyCourse.png"
               alt="Fotos Google Business"
               width={1000}
               height={1000}
