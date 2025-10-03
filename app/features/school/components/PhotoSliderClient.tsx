34f14273551eb9e84f20891b2adef44feff386c6
MarceTT <mtoro6@gmail.com>
2025-10-02 21:33:52 -0300

    fix(build): relax PhotoSlider props typing to match installed react-photo-view types

--- app/features/school/components/PhotoSliderClient.tsx
+++ app/features/school/components/PhotoSliderClient.tsx
@@ -2,10 +2,19 @@
 
 import React from "react";
 import { PhotoSlider as RPPhotoSlider } from "react-photo-view";
-import type { PhotoSliderProps } from "react-photo-view";
 import "react-photo-view/dist/react-photo-view.css";
 
-export default function PhotoSlider(props: PhotoSliderProps) {
-  return <RPPhotoSlider {...props} />;
-}
+type ImageItem = { src: string; key?: string; alt?: string };
+type Props = {
+  images: ImageItem[];
+  visible: boolean;
+  index: number;
+  onClose: () => void;
+  onIndexChange: (index: number) => void;
+  [key: string]: any;
+};
 
+export default function PhotoSlider(props: Props) {
+  // Tipado laxo para compatibilidad con la lib instalada
+  return <RPPhotoSlider {...(props as any)} />;
+}
